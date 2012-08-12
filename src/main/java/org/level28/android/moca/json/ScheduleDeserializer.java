6b268a1950416ca4d6a7002e26d28b06fdc70daa
Matteo Panella <morpheus@level28.org>
2012-08-12 12:49:45 +0200

    Reimplement Time.parse3339() in pure Java

--- src/main/java/org/level28/android/moca/json/ScheduleDeserializer.java
+++ src/main/java/org/level28/android/moca/json/ScheduleDeserializer.java
@@ -21,8 +21,12 @@
 
 package org.level28.android.moca.json;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -42,8 +46,6 @@ import com.google.common.collect.Maps;
  */
 public class ScheduleDeserializer extends
         AbstractJsonDeserializer<Map<String, Session>> {
-
-    private static final Time sTime = new Time();
 
     @Override
     public Map<String, Session> fromInputStream(InputStream in)
@@ -158,15 +160,164 @@ public class ScheduleDeserializer extends
             return result;
         } catch (IllegalArgumentException e) {
             throw new JsonDeserializerException("Invalid session entry", e);
+        } catch (ParseException e) {
+            throw new JsonDeserializerException("Invalid session entry", e);
         }
     }
 
-    private static long parseTime(String s) {
-        // XXX: parse3339 on all versions of Android up to (and including) Jelly
-        // Bean has some *SERIOUS* input validation issues. It HAS to be
-        // replaced by a saner (albeit slower) Java implementation of RFC3339.
-        // See also http://code.google.com/p/android/issues/detail?id=16002
-        sTime.parse3339(s);
-        return sTime.toMillis(false);
+    /**
+     * Pure java reimplementation of {@link Time#parse3339(String)}.
+     * 
+     * @see <a
+     *      href="http://code.google.com/p/android/issues/detail?id=16002">http://code.google.com/p/android/issues/detail?id=16002</a>
+     */
+    private static long parseTime(String timeString) throws ParseException {
+        checkNotNull(timeString, "Time input should not be null");
+        final int len = timeString.length();
+        checkArgument(len >= 10,
+                "Time input is too short; must be at least 10 characters");
+        final char[] s = timeString.toCharArray();
+
+        final Time t = new Time();
+        int n;
+        boolean inUtc = false;
+
+        // Year
+        n = getChar(s, 0, 1000);
+        n += getChar(s, 1, 100);
+        n += getChar(s, 2, 10);
+        n += getChar(s, 3, 1);
+        t.year = n;
+
+        // '-'
+        checkChar(s, 4, '-');
+
+        // Month
+        n = getChar(s, 5, 10);
+        n += getChar(s, 6, 1);
+        --n;
+        t.month = n;
+
+        // '-'
+        checkChar(s, 7, '-');
+
+        // Day
+        n = getChar(s, 8, 10);
+        n += getChar(s, 9, 1);
+        t.monthDay = n;
+
+        // Check if we have a time as well
+        if (len >= 19) {
+            // 'T'
+            checkChar(s, 10, 'T');
+            t.allDay = false;
+
+            // Hour
+            n = getChar(s, 11, 10);
+            n += getChar(s, 12, 1);
+            int hour = n;
+
+            // ':'
+            checkChar(s, 13, ':');
+
+            // Minute
+            n = getChar(s, 14, 10);
+            n += getChar(s, 15, 1);
+            int minute = n;
+
+            // ':'
+            checkChar(s, 16, ':');
+
+            // Second
+            n = getChar(s, 17, 10);
+            n += getChar(s, 18, 1);
+            t.second = n;
+
+            // Skip subsecond component (if any)
+            int tz_index = 19;
+            if (tz_index < len && s[tz_index] == '.') {
+                do {
+                    tz_index++;
+                } while (tz_index < len && s[tz_index] >= '0'
+                        && s[tz_index] <= '9');
+            }
+
+            int offset = 0;
+            if (len > tz_index) {
+                final char c = s[tz_index];
+
+                switch (c) {
+                case 'Z':
+                    // UTC
+                    offset = 0;
+                    break;
+                case '-':
+                    offset = 1;
+                    break;
+                case '+':
+                    offset = -1;
+                    break;
+                default:
+                    throw new ParseException("Unexpected character", tz_index);
+                }
+                inUtc = true;
+
+                if (offset != 0) {
+                    // Hour
+                    n = getChar(s, tz_index + 1, 10);
+                    n += getChar(s, tz_index + 2, 1);
+                    n *= offset;
+                    hour += n;
+
+                    // ':'
+                    checkChar(s, tz_index + 3, ':');
+
+                    // Minute
+                    n = getChar(s, tz_index + 4, 10);
+                    n += getChar(s, tz_index + 5, 1);
+                    n *= offset;
+                    minute += n;
+                }
+            }
+            t.hour = hour;
+            t.minute = minute;
+
+            if (offset != 0) {
+                t.normalize(false);
+            }
+        } else {
+            // We don't
+            t.allDay = true;
+            t.hour = 0;
+            t.minute = 0;
+            t.second = 0;
+        }
+
+        t.weekDay = 0;
+        t.yearDay = 0;
+        t.isDst = -1;
+        t.gmtoff = 0;
+
+        if (inUtc) {
+            t.timezone = Time.TIMEZONE_UTC;
+        }
+
+        return t.toMillis(false);
+    }
+
+    private static int getChar(final char[] s, int index, int multiplier)
+            throws ParseException {
+        final char c = s[index];
+        if (c >= '0' && c <= '9') {
+            return Character.digit(c, 10) * multiplier;
+        }
+        throw new ParseException("Illegal character", index);
+    }
+
+    private static void checkChar(final char[] s, int index, char expected)
+            throws ParseException {
+        if (s[index] != expected) {
+            throw new ParseException("Unexpected character", index);
+        }
     }
 }
